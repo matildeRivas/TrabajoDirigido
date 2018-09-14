5340d3934b5a1da1c9f2f445e1ed41eb928a5cb0
matildeRivas <matilde.rivas@ug.uchile.cl>
2018-09-14 11:44:46 -0300

    Add files via upload

---
+++ sementQuery.sql
@@ -0,0 +1,8 @@
+with                    
+	line_counts (cts, id) as 
+		(select ST_NPoints(geom) -1 , id from mapa_cobre),
+		series(num, id) as
+			(select generate_series(1, cts), id from line_counts)
+	select ST_MakeLine(ST_PointN(geom, num), ST_PointN(geom, num + 1)) as geom, mapa_cobre.id
+	from series
+inner join mapa_cobre on series.id = mapa_cobre.id;
