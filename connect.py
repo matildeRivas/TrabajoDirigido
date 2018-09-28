a28a7d90cce99667f42dbe5c1652b1dcf0926e12
matildeRivas <matilde.rivas@ug.uchile.cl>
2018-09-28 12:48:56 -0300

    Adds function to execute a query

--- connect.py
+++ connect.py
@@ -2,31 +2,42 @@ import psycopg2
 import sys
 import pprint
 import config
- 
+from queries import *
+
 def main():
+	executeQuery(connect(sys.argv[1]), pointsQuery)
+
+	
+#Connects to given database using credentials in config file
+#TODO return cursor
+def connect(dbname):
 	#Define our connection string
-	conn_string = "host='localhost' dbname='trabajodirigido' user='" + config.user + "' password=" + config.password
+	conn_string = "host='localhost' dbname='" + dbname +"' user='" + config.user + "' password=" + config.password
  
 	# print the connection string we will use to connect
-	print "Connecting to database\n	->%s" % (conn_string)
+	print("Connecting to database\n	->" + conn_string)
  
 	# get a connection, if a connect cannot be made an exception will be raised here
 	conn = psycopg2.connect(conn_string)
  
 	# conn.cursor will return a cursor object, you can use this cursor to perform queries
 	cursor = conn.cursor()
-	print "Connected!\n"
+	print('Connected!\n')
+
+	return cursor
+	
+	
+#Executes a query in a database, query is a string
+def executeQuery(cursor, query):
 	# execute our Query
-	cursor.execute("with line_counts (cts, id) as (select ST_NPoints(geom)-1, id from mapa_cobre), series(num, id) as (select generate_series(1, cts), id from line_counts) select ST_PointN(geom , num) as inicio, ST_PointN(geom, num + 1) as fin, mapa_cobre.id from series inner join mapa_cobre on series.id = mapa_cobre.id; ")
+	cursor.execute(query)
  
 	# retrieve the records from the database
 	records = cursor.fetchall()
  
 	# print out the records using pretty print
-	# note that the NAMES of the columns are not shown, instead just indexes.
-	# for most people this isn't very useful so we'll show you how to return
-	# columns as a dictionary (hash) in the next example.
 	pprint.pprint(records)
- 
+
+	
 if __name__ == "__main__":
 	main()
